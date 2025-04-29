d9f2fd993e970825e30eb8320733cb00b6daf2a1
Morgan Metz <morgan.metz@eyekon.xyz>
2025-04-29 11:33:29 -0400

    add mev lib base

---
+++ mev-lib/src/result.rs
@@ -0,0 +1,21 @@
+use std::fmt;
+
+pub type MevResult<T> = Result<T, MevError>;
+
+pub enum MevError {
+    ConversionWouldOverflow,
+    FailedToDeserialize,
+    FailedToSerialize,
+    UnknownError
+}
+
+impl fmt::Display for MevError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::ConversionWouldOverflow => write!(f, "Numeric conversion would overflow"),
+            Self::FailedToDeserialize => write!(f, "Failed to deserialize"),
+            Self::FailedToSerialize => write!(f, "Failed to serialize"),
+            Self::UnknownError => write!(f, "an Unknown Error occured")
+        }
+    }    
+}
